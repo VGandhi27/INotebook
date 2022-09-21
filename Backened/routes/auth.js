edf06c21a5480bfbb718a5f5ee3c85b871627ff0
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-09-22 00:02:11 +0530

    adding login endpoint

--- Backened/routes/auth.js
+++ Backened/routes/auth.js
@@ -49,10 +49,51 @@ router.post('/createuser',[
               // res.json({user})
               } catch (error) {
               console.error(error.message)
-              res.status(500).send("Some error occured ")
-              } 
+              res.status(500).send("Internal Server error occured ")
+       } 
 
        })
 
+    
 
+//Authenticate a User using:POST "api/auth/login " . -No login required
+router.post('/login',[
+       body('email','Enter a valid email').isEmail(),
+       body('password',' Password cannot be blank').exists(),
+],async (req,res)=>{
+ //if there are errors , return bad request and the errors
+ const errors = validationResult(req);
+ if (!errors.isEmpty()) {
+   return res.status(400).json({ errors: errors.array() });
+ }
+
+ const {email,password}=req.body;
+ try {
+       
+   let user= await User.findOne({email});
+   if(!user){
+       return res.status(400).json({error:"Please try to login again with correct credentials"});
+
+   }
+   const passwordCompare = await bcrypt.compare(password,user.password);
+ if(!passwordCompare){
+       return res.status(400).json({error:"Please try to login again with correct credentials"});
+
+ }
+ const data={
+       user:{
+              id: user.id
+       }
+       
+  }    
+  const autoken= jwt.sign(data ,JWT_SECRET);
+  res.json({autoken});
+
+ } 
+ catch (error) {
+       console.error(error.message)
+       res.status(500).send("Internal Server error occured ")
+       } 
+}
+)
 module.exports = router
