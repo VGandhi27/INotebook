ee1ad45e7356c85078e840c2c6c8101a8e05fbcd
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-09-24 10:42:26 +0530

    delete the notes

--- Backened/routes/notes.js
+++ Backened/routes/notes.js
@@ -40,9 +40,12 @@ router.post('/addnote', fetchuser, [
             res.status(500).send("Internal Server Error");
         }
     })
+
 // ROUTE 3: Update an existing Note using: Put "/api/notes/updatenote". Login required
 router.put('/updatenote/:id', fetchuser, async (req, res) => {
     const {title, description, tag} = req.body;
+   try {
+     
     // Create a newNote object
     const newNote  = {};
     if(title){newNote.title = title};
@@ -59,6 +62,36 @@ router.put('/updatenote/:id', fetchuser, async (req, res) => {
 
     note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
     res.json({note});
+
+   } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
 }
+    }
 )
+
+// ROUTE 4: Delete an existing Note using: Delete "/api/notes/deletenote". Login required
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+   try {
+    
+    // Find the note to be deleted and delete it
+    let note = await Note.findById(req.params.id);
+    if(!note){return res.status(404).send("Not Found")}
+
+    //Allow deletion only if user owns this note
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndDelete(req.params.id);
+    res.json({"Success":"Note has been deleted",note:note});
+
+   } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+}
+
+
+    
+})
 module.exports = router
