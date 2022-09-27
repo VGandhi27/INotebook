7ac5993cfcf48e723c8cda3bf7683a172d655cab
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-09-27 07:29:17 +0530

    adding cards

--- src/components.js/Home.js
+++ src/components.js/Home.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { useContext } from "react";
-import noteContext from "../context/notes/noteContext"
+import Notes from "./Notes";
 
 const Home = () => {
-  const context=useContext(noteContext);
-  const {notes,setNotes}=context;
   return (
     <>
       <div className="container my-2">
@@ -55,15 +52,7 @@ const Home = () => {
           </form>
         </div>
 
-        {/* Your Notes */}
-        <div className="container my-3">
-        <h1>Your Notes</h1>
-        {
-          notes.map((note)=>{
-            return note.title ;
-          })
-        }
-        </div>
+        <Notes/>
       </div>
     </>
   );
---
+++ src/components.js/Notes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
+import NoteItem from './NoteItem';
+
+const Notes = () => {
+    const context=useContext(noteContext);
+    const {notes,setNotes}=context;
+   
+  return (
+    <>
+    {/* Your Notes */}
+    <div className="row my-3">
+        <h1>Your Notes</h1>
+        {
+          notes.map((note)=>{
+            return <NoteItem note={note}/> ;
+          })
+        }
+        </div>
+    </>
+  )
+}
+
+export default Notes
