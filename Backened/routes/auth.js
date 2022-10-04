b256c46afd3cf0eacac1865c2ab9e40b15df1add
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-10-04 13:28:53 +0530

    add signup

--- Backened/routes/auth.js
+++ Backened/routes/auth.js
@@ -15,6 +15,8 @@ router.post('/createuser',[
        body('email','Enter a valid email').isEmail(),
        body('password','Enter a valid Password').isLength({min:5}),
 ],async (req,res)=>{
+       let success=false;
+
        //if there are errors , return bad request and the errors
        const errors = validationResult(req);
        if (!errors.isEmpty()) {
@@ -26,7 +28,7 @@ router.post('/createuser',[
        
        let user = await User.findOne({email: req.body.email});
        if(user){
-              return res.status(400).json({error:"Sorry a user with this email already exists "})
+              return res.status(400).json({success,error:"Sorry a user with this email already exists "})
        }
        const salt = await bcrypt.genSalt(10);
        const secPass= await bcrypt.hash(req.body.password,salt) ; 
@@ -44,8 +46,8 @@ router.post('/createuser',[
               
          }    
          const autoken= jwt.sign(data ,JWT_SECRET);
-        
-         res.json({autoken});
+        success=true;
+         res.json({success,autoken});
            // res.send(req.body);
               // res.json({user})
               } catch (error) {
