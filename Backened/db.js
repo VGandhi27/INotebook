2d662903adf6dd04e952f2c284c340da8c7dce65
Vidushi Gandhi <curiousvidushi27@gmail.com>
2023-09-04 20:06:23 +0530

    Update db.js

--- Backened/db.js
+++ Backened/db.js
@@ -4,7 +4,9 @@
  const moongoose=require('mongoose');
  
  
- const mongoURI ="mongodb://localhost:27017/inotebook"
+//  const mongoURI ="mongodb://localhost:27017/inotebook"
+ const mongoURI ="mongodb+srv://curiousvidushi27:Veenukush@cluster0.pa9g6yx.mongodb.net/?retryWrites=true&w=majority"
+  
 
  const connectToMongo =()=>{
     mongoose.connect(mongoURI,()=>{
